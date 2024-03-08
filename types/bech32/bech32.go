186ed67cc144d36ac9e15264925e03baba9d1e37
DedicatedDev <topdev323@gmail.com>
2024-03-08 11:41:51 -0600

    feat: change decode limit

--- types/bech32/bech32.go
+++ types/bech32/bech32.go
@@ -8,7 +8,7 @@ import (
 
 // ConvertAndEncode converts from a base256 encoded byte string to base32 encoded byte string and then to bech32.
 func ConvertAndEncode(hrp string, data []byte) (string, error) {
-	converted, err := bech32.ConvertBits(data, 8, 5, false)
+	converted, err := bech32.ConvertBits(data, 8, 5, true)
 	if err != nil {
 		return "", fmt.Errorf("encoding bech32 failed: %w", err)
 	}
@@ -18,7 +18,7 @@ func ConvertAndEncode(hrp string, data []byte) (string, error) {
 
 // DecodeAndConvert decodes a bech32 encoded string and converts to base256 encoded bytes.
 func DecodeAndConvert(bech string) (string, []byte, error) {
-	hrp, data, err := bech32.Decode(bech, 1023)
+	hrp, data, err := bech32.Decode(bech, 1000)
 	if err != nil {
 		return "", nil, fmt.Errorf("decoding bech32 failed: %w", err)
 	}
