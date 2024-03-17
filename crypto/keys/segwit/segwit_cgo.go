28cd5aa62d26c44f145fe76aebf7911273265ec7
liangping <18786721@qq.com>
2024-03-17 09:19:02 +0800

    improve VarintBufNum

--- crypto/keys/segwit/segwit_cgo.go
+++ crypto/keys/segwit/segwit_cgo.go
@@ -1,7 +1,6 @@
 package segwit
 
 import (
-	"encoding/base64"
 	"encoding/binary"
 
 	"github.com/cometbft/cometbft/crypto"
@@ -15,32 +14,25 @@ var MagicBytes = []byte("Bitcoin Signed Message:\n")
 func VarintBufNum(n uint64) []byte {
 	var buf []byte
 	if n < 253 {
-		// buf = Buffer.alloc(1);
-		// buf.writeUInt8(n, 0);
 		buf = make([]byte, 1)
 		buf[0] = byte(n)
 	} else if n < 0x10000 {
-		// buf = Buffer.alloc(1 + 2);
-		// buf.writeUInt8(253, 0);
-		// buf.writeUInt16LE(n, 1);
 		buf = make([]byte, 1+2)
 		buf[0] = 253
 		binary.LittleEndian.PutUint16(buf[1:], uint16(n))
 	} else if n < 0x100000000 {
-		// buf = Buffer.alloc(1 + 4);
-		// buf.writeUInt8(254, 0);
-		// buf.writeUInt32LE(n, 1);
 		buf = make([]byte, 1+4)
 		buf[0] = 254
 		binary.LittleEndian.PutUint32(buf[1:], uint32(n))
 	} else {
+		// This is original code from JS wallet, But it's not clear how to implement n & -1 in Go
 		// buf = Buffer.alloc(1 + 8);
 		// buf.writeUInt8(255, 0);
 		// buf.writeInt32LE(n & -1, 1);
 		// buf.writeUInt32LE(Math.floor(n / 0x100000000), 5);
 		buf = make([]byte, 1+8)
 		buf[0] = 255
-		binary.LittleEndian.PutUint32(buf[1:], uint32(n&1))
+		binary.PutVarint(buf[1:], int64(n)&-1) // n & -1, need to check
 		binary.LittleEndian.PutUint32(buf[5:], uint32(n/0x100000000))
 	}
 	return buf
@@ -50,12 +42,6 @@ func MagicHash(msg []byte) []byte {
 	return magicHash(msg)
 }
 func magicHash(msg []byte) []byte {
-
-	// const prefix1 = varintBufNum(MAGIC_BYTES.length);
-	// const messageBuffer = Buffer.from(message);
-	// const prefix2 = varintBufNum(messageBuffer.length);
-	// const buf = Buffer.concat([prefix1, MAGIC_BYTES, prefix2, messageBuffer]);
-	// return base.doubleSha256(buf);
 	prefix1 := VarintBufNum(uint64(len(MagicBytes)))
 	prefix2 := VarintBufNum(uint64(len(msg)))
 	buf := append(prefix1, MagicBytes...)
@@ -67,47 +53,29 @@ func magicHash(msg []byte) []byte {
 
 // Sign creates an ECDSA signature on curve Secp256k1, using SHA256 on the msg.
 func (privKey *PrivKey) Sign(msg []byte) ([]byte, error) {
-	// derivedKey := secp256k1.PrivKey{
-	// 	Key: privKey.Key,
-	// }
-	derivedKey, _ := btcec.PrivKeyFromBytes(privKey.Key)
 
+	derivedKey, _ := btcec.PrivKeyFromBytes(privKey.Key)
 	hash := MagicHash(msg)
 
-	println("hash:", hash)
-
-	sig, err := ecdsa.SignCompact(derivedKey, hash, true)
-	println("inner sig:", base64.StdEncoding.EncodeToString(sig))
-	return sig, err
-	// signature := ecdsa.Sign(derivedKey, hash)
-	// return signature.Serialize(), nil
-	//return derivedKey.Sign(magicHash(msg))
+	return ecdsa.SignCompact(derivedKey, hash, true)
 }
 
 // VerifySignature validates the signature.
 // The msg will be hashed prior to signature verification.
-func (pubKey *PubKey) VerifySignature(msg []byte, sigStr []byte) bool {
+func (pubKey *PubKey) VerifySignature(msg []byte, sigBytes []byte) bool {
 	pk, err := btcec.ParsePubKey(pubKey.Key)
 	if err != nil {
 		return false
 	}
-	println("gotSig:", base64.StdEncoding.EncodeToString(sigStr))
-	// hash := magicHash(msg)
-	// signature, err := ecdsa.ParseSignature(sigStr)
-	// if err != nil {
-	// 	return false
-	// }
-	// return signature.Verify(hash, pk)
 
-	println("sigStr:", sigStr)
 	hash := magicHash(msg)
-	gotPubKey, gotCompressed, err := ecdsa.RecoverCompact(sigStr, hash)
+	recoveredPK, compressed, err := ecdsa.RecoverCompact(sigBytes, hash)
 	if err != nil {
 		return false
 	}
-	if !gotCompressed {
+	if !compressed {
 		return false
 	}
-	return gotPubKey.IsEqual(pk)
+	return recoveredPK.IsEqual(pk)
 
 }
